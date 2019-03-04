4306a5b9fa02c0965862036fb208f4e77c7114ec
Wagner Kenji Yatabe <kenji.yatabe@gmail.com>
2019-03-03 22:58:02 -0300

    Mudança de encoding do arquivo data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_player_group VALUES (1, 'Liga da JustiÃ§a', 'https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml');
+INSERT INTO tb_player_group VALUES (1, 'Liga da Justiça', 'https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml');
 INSERT INTO tb_player_group VALUES (2, 'Os Vingadores', 'https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/vingadores.json');
